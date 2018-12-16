c54868e5229c5952a4548a188b113f8b28a4b461
Myung Kim <mjk287@cornell.edu>
2018-12-16 11:03:24 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {'Astrud Gilberto': 'Corcovado'};
 
 function updatePlaylist (playlist, artistName, songTitle) {
-  newPlaylist = Object.assign(playlist, {artistName : songTitle})
+  var newPlaylist = Object.assign(playlist, {artistName : songTitle})
   return newPlaylist
 }
